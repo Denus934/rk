dde133deea0901198a29350fdd9ba6b2ee3254c5
Denus934 <56293616+Denus934@users.noreply.github.com>
2019-11-26 10:58:43 +0300

    Create rk3.sql

---
+++ rk3.sql
@@ -0,0 +1,24 @@
+#Черныов Д.Д Вариант7
+CREATE TABLE IF NOT EXISTS rk3 (
+name VARCHAR(25) NOT NULL,
+manufacturer VARCHAR(25) NOT NULL,
+description VARCHAR(100) NOT NULL,
+count int(1200)
+);
+INSERT INTO rk3 (`name`, `manafacturer`, `discription`, `count`)
+VALUES
+('Lamp','Lamp','Lamp',0,),
+('TV','TV','TV',777,),
+('clock','clock','clock',1200,),
+('School','School','School',1129),
+('Russia','Russia','Russia',1,),
+('KABLE','KABLE','KABLE',0,),
+('Pen','Pen','Pen',670,),
+('Komp','Komp','Komp',500,),
+('Saha','Saha','Saha',980,),
+('Denis','Denis','Denis',1200,),
+('Dima','Dima','Dima',350,);
+//Задание 2
+DROP name FROM rk3 WHERE count=0;
+// Задание 3
+SELECT name  FROM rk3 WHERE name LIKE '%k' and count<1000 and >300;
